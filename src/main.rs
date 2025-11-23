0de480b834a9b1f1a6866754a12a90569f0874d1
Brendan McGuire <brendan@bren.app>
2025-11-23 12:50:44 -0600

    Add ratatui restore

--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,36 @@ mod editors;
 
 use crate::editors::Editor;
 
-#[derive(clap::Subcommand)]
+#[derive(Clone, clap::ValueEnum)]
 pub enum Commands {
-    Rebase {
-        /// Path to the rebase todo file
-        path: std::path::PathBuf,
-    },
+    Rebase,
 }
 
 #[derive(clap::Parser)]
 struct Cli {
-    #[clap(subcommand)]
-    command: Commands,
+    #[arg(short, long, global = true)]
+    command: Option<Commands>,
+
+    /// Path to the rebase todo file
+    path: std::path::PathBuf,
 }
 
 fn main() -> Result<()> {
     let args = Cli::try_parse()?;
     let terminal = ratatui::init();
 
-    match args.command {
-        Commands::Rebase { path } => {
-            let cwd = std::env::current_dir()?;
-            let path = if path.is_absolute() {
-                path
-            } else {
-                cwd.join(path).canonicalize()?
-            };
+    let cwd = std::env::current_dir()?;
+    let path = if args.path.is_absolute() {
+        args.path
+    } else {
+        cwd.join(args.path).canonicalize()?
+    };
 
-            let mut editor = editors::rebase::RebaseEditor::new(path)?;
-            editor.run(terminal)
-        }
-    }
+    let mut editor: Box<dyn Editor> = match args.command {
+        Some(Commands::Rebase) | None => Box::new(editors::rebase::RebaseEditor::new(path)?),
+    };
+    let result = editor.run(terminal);
+
+    ratatui::restore();
+    result
 }
